4bb72925d9d8da3b6b8f45cfb27d7a5086aec2f5
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-05-31 16:25:38 +0900

    Chapter2-3-Disposable

---
+++ Chapter2-3-Disposable.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+import RxSwift
+
+// Disposed
+// 모든 리소스가 정리된후에 호출됨
+let subscription1 = Observable.from([1, 2, 3])
+    .subscribe(onNext: { elem in
+        print("Next", elem)
+    }, onError: { error in
+        print("Error", error)
+    }, onCompleted: {
+        print("Completed")
+    }, onDisposed: {
+        print("Disposed")
+    })
+
+subscription1.dispose() // 리소스해제
+
+var bag = DisposeBag() // dispose보다 DisposeBag 사용을 추천
+
+// Disposed 호출되지 않는다.
+// 호출되지 않는다고해서 리소스가 정리되지않은것은 아니다
+Observable.from([1, 2, 3])
+    .subscribe {
+        print($0)
+    }
+    .disposed(by: bag) // disposeBag에 담김
+
+bag = DisposeBag() // 이전의 disposeBag 해제
+
+let subscription2 = Observable<Int>.interval (.seconds(1),
+                                              scheduler: MainScheduler.instance)
+    .subscribe(onNext: { elem in
+        print("Next", elem)
+    }, onError: { error in
+        print("Error", error)
+    }, onCompleted: {
+        print("Completed")
+    }, onDisposed: {
+        print("Disposed")
+    })
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+    subscription2.dispose()
+}
