b7eb3431bcf9a19021bdf9bfae4d76f50275e348
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-06-06 02:31:57 +0900

    Chapter6-4-flatMap

---
+++ Chapter6-4-flatMap.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+import RxSwift
+
+/*:
+ ## FlatMap - Transforming Operator
+ * Inner Obesrvable
+ * Flatining Observable
+ */
+
+let disposeBag = DisposeBag()
+
+let redCircle = "🔴"
+let greenCircle = "🟢"
+let blueCircle = "🔵"
+
+let redHeart = "❤️"
+let greenHeart = "💚"
+let blueHeart = "💙"
+
+// Observable이 방출하는 항목을 Inner Observable을 통해서 평탄화하는 작업?
+Observable.from([redCircle, greenCircle, blueCircle])
+    .flatMap { circle -> Observable<String> in
+        switch circle {
+        case redCircle:
+            return Observable.repeatElement(redHeart)
+                .take(5)
+        case greenCircle:
+            return Observable.repeatElement(greenHeart)
+                .take(5)
+        case blueCircle:
+            return Observable.repeatElement(blueHeart)
+                .take(5)
+        default:
+            return Observable.just("")
+        }
+    }
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
