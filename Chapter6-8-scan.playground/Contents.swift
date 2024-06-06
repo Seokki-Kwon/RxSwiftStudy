4734c05dffc409d21e4a64d50de51ae633ceeb3a
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-06-06 11:26:38 +0900

    Chapter6-8-scan

---
+++ Chapter6-8-scan.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+import RxSwift
+/*:
+  ## scan - Transforming Operator
+ * Accumlator Function
+ * 기본값으로 연산시작
+ * 원본이 방출하는 대상으로 결과를 방출하는 하나의 옵저버블리턴
+ */
+
+let disposeBag = DisposeBag()
+
+// 중간값이 필요한경우 사용한다. 
+Observable.range(start: 1, count: 10)
+//    .scan(0, accumulator: +)
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
