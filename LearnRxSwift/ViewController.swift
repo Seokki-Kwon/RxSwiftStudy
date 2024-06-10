5d55968ae41cf06935016c67321f670d220d6396
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-06-11 01:19:34 +0900

    Start RxCocoa

--- LearnRxSwift/ViewController.swift
+++ LearnRxSwift/ViewController.swift
@@ -7,13 +7,73 @@
 
 import UIKit
 
+struct Chapter {
+    let title: String
+    let screen: UIViewController
+}
 class ViewController: UIViewController {
-
+    let myClass = [
+        Chapter(title: "Rxcocoa-01-Hello RxCocoa", screen: HelloRxCocoaViewController())
+    ]
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        tableView.register(MyCell.self, forCellReuseIdentifier: MyCell.reuseIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.frame = view.bounds
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        myClass.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MyCell.reuseIdentifier, for: indexPath) as? MyCell else { return UITableViewCell()
+        }
+        
+        cell.title = myClass[indexPath.row].title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        navigationController?.pushViewController(myClass[indexPath.row].screen, animated: true)
+    }
+}
+
+class MyCell: UITableViewCell {
+    static let reuseIdentifier = "myCell"
+    var title: String? {
+        didSet {
+            titleLabel.text = title
+        }
+    }
+    private let titleLabel: UILabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        
+    }
+}
---
+++ RxCocoaBasic/HelloRxCocoaViewController.swift
@@ -0,0 +1,51 @@
+//
+//  HelloRxCocoaViewController.swift
+//  LearnRxSwift
+//
+//  Created by 석기권 on 6/11/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class HelloRxCocoaViewController: UIViewController {
+    let bag = DisposeBag()
+    
+    private var valueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello"
+        return label
+    }()
+    
+    private var tapButton: UIButton = {
+        let bt = UIButton()
+        bt.translatesAutoresizingMaskIntoConstraints = false
+        bt.setTitle("Tap", for: .normal)
+        bt.backgroundColor = .red
+        return bt
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(valueLabel)
+        view.addSubview(tapButton)
+        
+        valueLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        valueLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        tapButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        tapButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
+        view.backgroundColor = .systemBackground
+        
+        tapButton.rx.tap
+            .map { "Hello RxCocoa" }
+//            .subscribe { [weak self] str in
+//                self?.valueLabel.text = str
+//            }
+            .bind(to: valueLabel.rx.text)
+            .disposed(by: bag)
+    }
+}
+
+
