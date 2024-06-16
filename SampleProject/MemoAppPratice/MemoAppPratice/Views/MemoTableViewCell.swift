1df84c876ec4720ad3ece829b387b908d5406073
Seokki-Kwon <gkwon1120@icloud.com>
2024-06-17 02:09:50 +0900

    Feat: MemoStorage 추가

---
+++ SampleProject/MemoAppPratice/MemoAppPratice/Views/MemoTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MemoTableViewCell.swift
+//  MemoAppPratice
+//
+//  Created by 석기권 on 6/17/24.
+//
+
+import UIKit
+
+class MemoTableViewCell: UITableViewCell {
+    static let reuseIdentifier = "MemoCell"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
